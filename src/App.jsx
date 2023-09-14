c926d7cc7ca2a9b5a4ec6204abd70aa5438e424c
h4ze-07 <vlad0109031@gmail.com>
2023-09-14 12:48:56 +0300

    ended task

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,84 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import SmileCard from "./comonents/SmileCard"
-import { data } from "./db/data";
 
 function App() {
 
-  const [dataFromBc, setDataFromBc] = useState([])
-
-  useEffect(() => {
-    setDataFromBc(data)
-  }, [])
+  const [smile1, setSmile1] = useState(0)
+  const [smile2, setSmile2] = useState(0)
+  const [smile3, setSmile3] = useState(0)
 
   const [arr, setArr] = useState([])
 
+  const [winner, setWinner] = useState(null)
+
+  const handleClick = () => {
+
+    let maxValue = 0;
+
+    smiles.forEach((smile) => {
+      smile.counter >= maxValue ? maxValue = smile.counter : null
+    })
+
+    smiles.forEach((smile) => {
+      if (maxValue === smile.counter) {
+        setWinner(smile)
+      }
+    })
+
+    setSmile1(0)
+    setSmile2(0)
+    setSmile3(0)
+    maxValue = 0
+  }
+
+  const handleWinner = () => {
+    setWinner(null)
+  }
+
+  const smiles = [
+    {
+      id: 1,
+      smile: '😊',
+      counter: smile1,
+      func: setSmile1,
+    },
+    {
+      id: 2,
+      smile: '😒',
+      counter: smile2,
+      func: setSmile2,
+    },
+    {
+      id: 3,
+      smile: '❤️',
+      counter: smile3,
+      func: setSmile3,
+    }
+  ]
 
   return (
     <>
       <main>
         <h2 className="title">Smile rating</h2>
-        <div className="container">
-          {dataFromBc.map((item) => (
-            <SmileCard key={item.id} smile={item.smile} />
-          ))}
-        </div>
-        <button className="winner"
-        
-        >See a winner!</button>
-        <span>{arr}</span>
+        {!winner &&
+          <div>
+            <div className="container">
+              {smiles.map((smile) => (
+                <SmileCard key={smile.id} smile={smile} />
+              ))}
+            </div>
+            <button className="winner"
+            onClick={handleClick}
+            >See a winner!</button>
+          </div>
+        }
+        {winner && 
+        <div className="smileContainer">
+          <h2 className="title">WINNER!</h2>
+          <p>Total clicks: {winner.counter}</p>
+          <p className="smile">{winner.smile}</p>
+          <button className="rateBtn" onClick={handleWinner}>Got it</button>
+        </div>}
       </main>
     </>
   )
--- src/comonents/SmileCard.jsx
+++ src/comonents/SmileCard.jsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
 
-const SmileCard = ({smile, handler}) => {
-
-    const [counter, setCounter] = useState(0)
+const SmileCard = ({smile}) => {
 
     const handleClick = () => {
-        setCounter(counter + 1)
-        handler(counter + 1)
+        smile.func(pr => pr + 1)
     }
 
     return (
         <div className="smileContainer">
-            <h2 className="smile">{smile}</h2>
-            <h3>{counter}</h3>
+            <h2 className="smile">{smile.smile}</h2>
+            <h3>{smile.counter}</h3>
             <button className="rateBtn" 
             onClick={handleClick}
             >Rate it!</button>
